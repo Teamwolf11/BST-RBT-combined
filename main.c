33cf56d77c8a9746bf78bbc04d0bcd2558a4c3ba
Mike <63983972+Teamwolf11@users.noreply.github.com>
2020-09-12 16:23:10 +1200

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <getopt.h>
 #include "tree.h"
 #include "mylib.h"
+
+static tree_t type; 
 
 static void print_info(int freq, char *word) {
     printf("%-4d %s\n", freq, word);
@@ -14,25 +16,63 @@ void print_key(char *s){
 }
 
 int main(int argc, char **argv) {
-
+    
+    tree b;
     const char *optstring = "c:df:orh";
     char option;
-    
+
+    type = BST;
+
     while ((option = getopt(argc, argv, optstring)) != EOF) {
         switch (option) {
             case 'c':
-                
                 /* do something */
+               
+                    break;
             case 'd':
                 /* the argument after the -b is available
                    in the global variable 'optarg' */
+
+
+                /* tree_depth(b); */
+                printf("print tree depth");
+                exit(EXIT_SUCCESS);
+                break;
             case 'f':
                 /* do something else */
+                break;
+            case 'o':
+                break;
+            case 'r':
+                type = RBT;
+                break;
+            case 'h':
             default:
                 /* if an unknown option is given */
+                if(option != 'h'){
+                printf("unknown command line arg was given: -");
+                printf("%c\n",option);
+                }
+                printf("Perform various operations using a binary tree.  By default, words\n");
+                printf("are read from stdin and added to the tree, before being printed out\n");
+                printf("alongside their frequencies to stdout.\n");
+                printf("\n");
+                printf("-c FILENAME  Check spelling of words in FILENAME using words\n");
+                printf("             read from stdin as the dictionary.  Print timing\n");
+                printf("             info & unknown words to stderr (ignore -d & -o)\n");
+                printf("-d           Only print the tree depth (ignore -o)\n");
+                printf("-f FILENAME  Write DOT output to FILENAME (if -o given)\n");
+                printf("-o           Output the tree in DOT form to file 'tree-view.dot'\n");
+                printf("-r           Make the tree an RBT (the default is a BST)\n");
+                printf("\n");
+                printf("-h           Print this message\n\n");
+
+                exit(EXIT_FAILURE);
         }
     }
-    tree b= tree_new(RBT);
+    if(type == RBT) printf("RBT\n");
+    
+    b = tree_new(type);
     b=tree_insert(b,"h");
     b=tree_insert(b,"l");
     b=tree_insert(b,"i");
