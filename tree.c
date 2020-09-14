551859bf0a3c26d0b8f72874cf2954a9e888522f
alari453 <64070727+alari453@users.noreply.github.com>
2020-09-14 17:06:46 +1200

    Update tree.c
    nothing over 80 characters

--- tree.c
+++ tree.c
@@ -10,10 +10,10 @@
 static tree_t tree_type;
 
 /**
- * tree node
- * key - value
- * left - pointer to left child node
- * right - pointer to right child node
+ * tree node.
+ * key - value.
+ * left - pointer to left child node.
+ * right - pointer to right child node.
  **/
 struct tree_node { 
     char *key;
@@ -47,21 +47,19 @@ tree tree_new(tree_t t) {
  * @return 0 if not found, 1 if found.
  **/
 int tree_search(tree r, char *key) {
+    /* returns 0 if strings are equal.*/   
     if (r == NULL) {
         return 0;
     }
-    /* strcmp returns 0 if strings are equal, 1 if the current key is bigger than the key being searched, and negative if
-     * the current key is less than the key being searched
-     */
 
-    /* If the searched key is greater, search left tree instead */
+    /* If the searched key is greater, search left tree instead.*/
     if (strcmp(r->key, key) > 0) {
         return tree_search(r->left, key);
-        /* If the searched key is less than, search right tree instead */
+    /* If the searched key is less than, search right tree instead.*/
     } else if(strcmp(r->key, key) < 0) {
         return tree_search(r->right, key);
     }
-    /* If the keys are equal, return 1 */
+    /* If the keys are equal, return 1.*/
     return 1;
 }
 
@@ -72,7 +70,7 @@ int tree_search(tree r, char *key) {
  * @return r - the modified tree.
  **/
 tree recursive_insert (tree r, char *key){
-    /*If an empty tree, we can allocate memory, copy in the key into the node, and return the result. */
+    /*if empty, allocate memory, copy key into node, return result.*/
     if(r == NULL) {
         r = emalloc(sizeof *r);
         r->key = emalloc((strlen(key) + 1) * sizeof key[0]);
@@ -84,18 +82,20 @@ tree recursive_insert (tree r, char *key){
         return r;
     }
 
-    /* If the current top node value is greater than key being inserted, then we insert into the left subtree */
+    /*if current top node is greater than key, then insert into left subtree.*/
     if(strcmp(r -> key, key) > 0) {
         r->left = recursive_insert(r->left, key);
-        /* else if the current top node value is less than key being inserted, then we insert into the right subtree */
+    /*if the current top node is less than key, then insert into right subtree */
     } else if(strcmp(r -> key, key) < 0) {
         r->right = recursive_insert(r->right, key);
     } else {
         r->frequency++;
     }
+
+    /*when an RBT, run the fix function.*/
     if(tree_type == RBT){
         r = tree_fix(r);
-    } /* this is for when it is an RBT it will run the fix function */
+    }
     
     /* return modified tree */
     return r;
@@ -113,35 +113,32 @@ tree tree_insert(tree r, char *key) {
     return r;
 }
 /**
- * The fix method looks downwards through the nodes to see
+ * The fix method looks downwards through the nodes to see.
    if any children/grandchildren pairs are both red.
  * If this happens then rotations and re-colourings must occur.
  * @param r - tree to use/fix.
  * @return r - the modified tree.
  **/
 tree tree_fix (tree r){
-    if(IS_RED(r->left)){ /* If left child of root (A) is red */
-    
-        if(IS_RED(r->left->left)){ /* and if left-left child of root (C) is red, LL */ 
-            if(IS_RED(r->right)){ /* and if the right child of root (B, the uncle of C) is red, R */
-                r->colour = RED; /* set root (R) to red */
-                r->left->colour = BLACK; /* set left child of root (A) to black */
-                r->right->colour = BLACK; /* set right child of root (B) to black */
-                
-            }else { /* else if the right child of root (B, the uncle of C) is black, R */
-                r = right_rotate(r); /* right-rotate root (R) */
-                r->colour = BLACK; /* set new root (A) to black */
-                r->right->colour = RED; /* set old root (new child, R) to red */
+    if(IS_RED(r->left)){ 
+        if(IS_RED(r->left->left)){ 
+            if(IS_RED(r->right)){ 
+                r->colour = RED; 
+                r->left->colour = BLACK;
+                r->right->colour = BLACK;
+            }else {
+                r = right_rotate(r); 
+                r->colour = BLACK; 
+                r->right->colour = RED; 
             } /*R*/
         } /*LL*/
         
-        if(IS_RED(r->left->right)){/*when z is right and is red LR */
-            if(IS_RED(r->right)){/*when uncle is red R*/
+        if(IS_RED(r->left->right)){
+            if(IS_RED(r->right)){
                 r->colour = RED;
                 r->left->colour = BLACK;
                 r->right->colour = BLACK;
-                
-            }else {/*when uncle is BLACK R*/
+            }else {
                 r->left = left_rotate(r->left);
                 r = right_rotate(r);
                 r->colour = BLACK;
@@ -150,14 +147,13 @@ tree tree_fix (tree r){
         }/*LR*/
     }/*L*/
 
-    if(IS_RED(r->right)){/* lets check if right is red*/
-        if(IS_RED(r->right->left)){/*when z is left and parent is left and is red RL*/ 
-            if(IS_RED(r->left)){/*when uncle is red L*/
+    if(IS_RED(r->right)){
+        if(IS_RED(r->right->left)){
+            if(IS_RED(r->left)){
                 r->colour = RED;
                 r->left->colour = BLACK;
                 r->right->colour = BLACK;
-                
-            }else {/*when uncle is BLACK L*/
+            }else {
                 r->right = right_rotate(r->right);
                 r = left_rotate(r);
                 r->colour = BLACK;
@@ -165,13 +161,13 @@ tree tree_fix (tree r){
             }/*L*/
         }/*RL*/
         
-        if(IS_RED(r->right->right)){/*when z is right and parent is left and is red RR */
-            if(IS_RED(r->left)){/*when uncle is red L*/
+        if(IS_RED(r->right->right)){
+            if(IS_RED(r->left)){
                 r->colour = RED;
                 r->left->colour = BLACK;
                 r->right->colour = BLACK;
                 
-            }else {/*when uncle is BLACK L*/
+            }else {
                 r = left_rotate(r);
                 r->colour = BLACK;
                 r->left->colour = RED;
@@ -218,22 +214,22 @@ tree left_rotate(tree r){
  * @param f - print function.
  **/
 void tree_inorder(tree r, void f(char *s)) {
-    /* if tree is null then return, stopping */
+    /* if tree is null then return, stopping.*/
     if(r == NULL) {
         return;
     }
-    /* call self on left subtree, traversing recursively */
+    /* call self on left subtree, traversing recursively.*/
     tree_inorder(r->left, f);
 
-    /* if (IS_RED(r)) { */
-    /*     printf("red:\t"); */
-    /* } else { */
-    /*     printf("black:\t"); */
-    /* } */
+    if (IS_RED(r)) {
+        printf("red:\t");
+    } else {
+        printf("black:\t");
+    }
     
-    /* call print method on current node */
+    /* call print method on current node.*/
     f(r->key);
-    /* call self on right subtree, traversing recursively */
+    /* call self on right subtree, traversing recursively.*/
     tree_inorder(r->right, f);
 }
 
@@ -243,18 +239,18 @@ void tree_inorder(tree r, void f(char *s)) {
  * @param f - print function.
  **/
 void tree_preorder(tree r, void f(int freq, char *s)) {
-    /* if tree is null then return, stopping */
+    /* if tree is null then return, stopping.*/
     if(r == NULL) {
         return;
     }
-    /* if (IS_RED(r)) { */
-    /*     printf("red:\t"); */
-    /* } else { */
-    /*     printf("black:\t"); */
-    /* } */
-    /* call print method on current node */
+    if (IS_RED(r)) {
+        printf("red:\t");
+    } else {
+        printf("black:\t");
+    }
+    /* call print method on current node.*/
     f(r->frequency,r->key);
-    /* call self on left and right, traversing subtree recursively */
+    /* call self on left and right, traversing subtree recursively.*/
     tree_preorder(r->left, f);
     tree_preorder(r->right, f);
 }
@@ -273,11 +269,11 @@ int tree_depth(tree r){
         return -1;
     else
         {
-            /* compute the depth of each subtree */
+            /* compute the depth of each subtree/*/
             lDepth = tree_depth(r->left);
             rDepth = tree_depth(r->right);
   
-            /* use the larger one */
+            /* use the larger one.*/
             if (lDepth > rDepth){
                 return(lDepth+1);
             }else{
@@ -292,11 +288,11 @@ int tree_depth(tree r){
  * @return NULL - a null pointer.
  **/
 tree tree_free(tree r) {
-    /* if tree is null then return, stops */
+    /* if tree is null then return, stops.*/
     if(r == NULL) {
         return NULL;
     }
-    /* free left and right subtrees recursively */
+    /* free left and right subtrees recursively.*/
     tree_free(r -> left);
     tree_free(r -> right);
 
