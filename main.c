28da6448f2357e78d24cf5b0eb3eb1d1023f3151
alari453 <64070727+alari453@users.noreply.github.com>
2020-09-14 16:36:58 +1200

    Update main.c

--- main.c
+++ main.c
@@ -119,6 +119,7 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Unknown Words = %d\n", unknownWord);
 
         fclose(filename);
+    }
         
 
     /*if is_d, is_c ignored print the tree_depth.*/
