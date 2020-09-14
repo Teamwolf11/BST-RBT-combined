a554250a2c098e9d11e6d6aacd41af0dcdb46bbc
Mike <63983972+Teamwolf11@users.noreply.github.com>
2020-09-14 15:27:42 +1200

    Update main.c

--- main.c
+++ main.c
@@ -18,6 +18,15 @@ static void print_info(int freq, char *word) {
     printf("%-4d %s\n", freq, word);
 }
 
+
+/* /\** */
+/*  * @param s. */
+/*  **\/ */
+/* void print_key(char *s){ */
+/*     printf("%5s\n",s); */
+/* } */
+
+
 /**
  * tree main function.
  * @param argc - argument count.
@@ -28,9 +37,13 @@ int main(int argc, char **argv) {
     tree b;
     const char *optstring = "c:df:orh";
     char option;
+    /* int i; */
     FILE *filename;
     char wordOnDictionary[WORDSIZE];
+    /* int wordcountDictionary; */
     char word[WORDSIZE];
+    /* char **listToCheck= NULL; */
+    /* int countList; */
     int  is_c =0;
     int depth;
     int is_d_called =0;
@@ -39,7 +52,9 @@ int main(int argc, char **argv) {
     clock_t start, end;
     int unknownWord = 0;
     double fillTime = 0;
-
+    FILE *dotFile;
+    FILE *customDotFile;
+    
     /* default BST. */
     type = BST;
  
@@ -61,11 +76,11 @@ int main(int argc, char **argv) {
             case 'f':
                 /* do something else */
                 is_f = 1;
-
+                customDotFile = fopen(optarg, "w");                                      
                 break;
             case 'o':
                 is_o =1;
-                printf("-o works");                
+                printf("-o works\n");                
                 break;
             case 'r':
                 /* tree type becomes RBT. */
@@ -96,15 +111,13 @@ int main(int argc, char **argv) {
 
     /*-------------------------------------------------------------------------------------------------------------*/   
     /*this will get all the words from the dictionary*/
-    start = clock();
     while (getword(wordOnDictionary, WORDSIZE, stdin) != EOF) {
         /*this will insert dictionary words into tree but this has to happen before the comparison step*/
         b = tree_insert(b,wordOnDictionary);
     } /* end while */
-    end = clock();
-    fillTime = (end - start) / (double)CLOCKS_PER_SEC;
     /*-------------------------------------------------------------------------------------------------------------*/   
 
+    
     /*-------------------------------------------------------------------------------------------------------------*/   
     if(is_c == 1){    
         /**checks spelling of words from file using words passed to stdin as the dictionary.**/
@@ -124,25 +137,44 @@ int main(int argc, char **argv) {
             }
         }/*end while loop for going through all the words in the filetobespellchecked (aka filename)*/
 
+        end = clock();
+        fillTime = (end - start) / (double)CLOCKS_PER_SEC;
+        
         /**print timing information**/
-        fprintf(stderr, "Fill time     : %.6f\n Search Time   : %.6f\n", fillTime,
+        fprintf(stderr, "Fill time     : %.6f\nSearch Time   : %.6f\n", fillTime,
                 (clock() - start) / (double)CLOCKS_PER_SEC);
         fprintf(stderr, "Unknown Words = %d\n", unknownWord);
+        
+
 
         fclose(filename);
         
     }/** if the is_c ==1 */
     /*-------------------------------------------------------------------------------------------------------------*/   
-   
-    /*-------------------------------------------------------------------------------------------------------------*/   
+    
+    
+     
     /*if is_d_called and is_c ignored print the tree_depth.*/
     if(is_c == 0 && is_d_called == 1){
         depth = tree_depth(b); 
         printf("print tree depth: %d\n", depth);
         exit(EXIT_SUCCESS);
     }
-    /*-------------------------------------------------------------------------------------------------------------*/
-    /* tree_preorder(b, print_info); */
+  
+    if(is_o == 1 && is_c == 0){
+        dotFile = fopen("tree-view.dot", "w");
+        printf("Creating dot file 'tree-view.dot'\n");
+        tree_output_dot(b,dotFile); 
+        fclose(dotFile);
+       
+    }
+
+    if(is_f == 1 && is_o == 1 && is_c == 0){
+        printf("creating new custom file xD");
+        
+    }
+  
+    /* tree_preorder(b, print_info );*/
     
     /* tree_preorder(b, print_key); */
     /* printf("DIVIDE\n"); */
